ad2b6d9739ec345404e7db8c7d98226b87a059a5
shivakumar <shivakumar27342@gmail.com>
2022-09-21 17:05:03 +0530

    updated

---
+++ dist_alx/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_alx',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_alx'],
+      author_email='mihajlovic.aleksa@gmail.com',
+      zip_safe=False)
